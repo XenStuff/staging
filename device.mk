9799646dc5ff6eeef1e987882ee1414082492e7b
AmulyaX <amulya.b520@gmail.com>
2019-08-12 18:44:35 +0530

    lavender: Light: Import HAL
    - Import the HAL from LineageOS/android_device_xiaomi_msm8998-common &
    - Relevant changes in selinux file_contexts.
    - This includes the recent fixes from upstream and makes
      it much cleaner. Note, I removed the chown/chmods because
      they are already done in init.rc/ueventd.rc/init.qcom.rc.
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Wang Han <416810799@qq.com>
    Co-authored-by: Atman <masteratman@gmail.com>
    Signed-off-by: Atman <masteratman@gmail.com>
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- device.mk
+++ device.mk
@@ -173,6 +173,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_lavender
+
 # MIDI feature
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
 
