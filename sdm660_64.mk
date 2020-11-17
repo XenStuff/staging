0de7c51a6755f6dfa2b7a6e70b317cec395c7fcc
Vinoth Jayaram <c_vinoja@codeaurora.org>
2020-11-17 18:03:30 +0530

    sdm660_64 : Updated Mapper & Allocator
    Updated Mapper & Allocator version.
    Enabled Gralloc4 target flag.

--- sdm660_64.mk
+++ sdm660_64.mk
@@ -264,10 +264,18 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 #Display/Graphics
+ifeq (true,$(call math_gt_or_eq,$(SHIPPING_API_LEVEL),29))
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.display.allocator-service \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display
+else
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.mapper@2.0-impl-2.1
+endif
+PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
