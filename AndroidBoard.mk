102315a67ad3c6d4055f3d3ad499e554ca09a3e0
Eruvaram Kumar Raja Reddy <erajared@codeaurora.org>
2020-12-07 13:53:57 +0530

    sdm660_64 : Fix compilation issue due to ufs changes
    We see compilation issues due to UFS changes after
    Disabling DAP and new launch features.
    Update fstab.default with fstab.emmc to fix this issue.
    
    Change-Id: If9b4a454bbd4db868be105fed6c9998bf83ce168

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -164,7 +164,7 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.default
+LOCAL_MODULE       := fstab.emmc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 ifeq ($(ENABLE_AB), true)
