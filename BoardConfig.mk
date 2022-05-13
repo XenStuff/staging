2b2d8af2f6362599412042c9a5e364abb661c346
xenxynon <flynryder427@gmail.com>
2022-05-13 10:11:30 +0000

    lavender: add lavender specific properties
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,6 +168,10 @@ BOARD_USES_QCOM_HARDWARE := true
 # Power
 TARGET_POWERHAL_VARIANT := qcom
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # RIL
 TARGET_RIL_VARIANT := caf
 
