9762c65588ff3ed72de30d9f97ce1bc2046f2e4d
xenxynon <flynryder427@gmail.com>
2022-05-13 07:12:32 +0000

    lavender: cleanup moar unnecessary flags
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- device.mk
+++ device.mk
@@ -10,32 +10,14 @@ TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 DEVICE_PACKAGE_OVERLAYS += \
        $(LOCAL_PATH)/overlay
 
-# Audio
-TARGET_USES_AOSP_FOR_AUDIO := false
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-TARGET_DISABLE_DASH := true
-
-# enable the SVA in UI area
-TARGET_USE_UI_SVA := true
-
-#QTIC flag
--include $(QCPATH)/common/config/qtic-config.mk
-
-# Add soft home, back and multitask keys
-PRODUCT_PROPERTY_OVERRIDES += \
-    qemu.hw.mainkeys=0
-
 # Video codec configuration files
-ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
-    $(LOCAL_PATH)/configs/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml 
-
-endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
+    $(LOCAL_PATH)/configs/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml
 
 # video seccomp policy files
 PRODUCT_COPY_FILES += \
@@ -45,10 +27,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.video.disable.ubwc=1
-
-ifneq ($(TARGET_DISABLE_DASH), true)
-    PRODUCT_BOOT_JARS += qcmediaplayer
-endif
 
 # Power
 PRODUCT_PACKAGES += \
@@ -73,7 +51,6 @@ PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, device/qcom/common/common64.mk)
 
 PRODUCT_NAME := sdm660_64
 PRODUCT_DEVICE := sdm660_64
@@ -87,38 +64,8 @@ TARGET_USES_MEDIA_EXTENSIONS := true
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610
 
-#Android EGL implementation
-PRODUCT_PACKAGES += libGLES_android
 PRODUCT_BOOT_JARS += tcmiface
 PRODUCT_BOOT_JARS += telephony-ext
-
-PRODUCT_PACKAGES += telephony-ext
-
-ifneq ($(strip $(QCPATH)),)
-PRODUCT_BOOT_JARS += WfdCommon
-#Android oem shutdown hook
-#PRODUCT_BOOT_JARS += oem-services
-endif
-
-DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
-ifeq ($(strip $(TARGET_KERNEL_VERSION)), 4.19)
-  DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest_soundtrigger.xml
-endif
-
-ifeq ($(strip $(SHIPPING_API_LEVEL)), 30)
-  DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest_target_level_5.xml
-else ifeq ($(strip $(SHIPPING_API_LEVEL)), 29)
-  DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest_target_level_4.xml
-else
-  DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest_target_level_3.xml
-endif
-DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(LOCAL_PATH)/framework_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
-
-# Audio configuration file
--include $(TOPDIR)hardware/qcom/audio/configs/sdm660/sdm660.mk
--include $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/sdm660/sdm660.mk
 
 USE_LIB_PROCESS_GROUP := true
 
@@ -241,18 +188,8 @@ PRODUCT_COPY_FILES += frameworks/native/data/etc/android.software.midi.xml:syste
 
 # MSM IRQ Balancer configuration file for SDM660
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # MSM IRQ Balancer configuration file for SDM630
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/msm_irqbalance_sdm630.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_sdm630.conf
-
-ifneq ($(BOARD_AVB_ENABLE), true)
-  # dm-verity configuration
-  PRODUCT_SUPPORTS_VERITY := true
-  PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
-  ifeq ($(ENABLE_VENDOR_IMAGE), true)
-    PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/bootdevice/by-name/vendor
-  endif
-endif
 
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 
@@ -261,25 +198,6 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 #for android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
-
-# Add the overlay path
-#PRODUCT_PACKAGE_OVERLAYS := $(QCPATH)/qrdplus/Extension/res \
-#       $(QCPATH)/qrdplus/globalization/multi-language/res-overlay \
-#      $(PRODUCT_PACKAGE_OVERLAYS)
-
-# Enable logdumpd service only for non-perf bootimage
-ifeq ($(findstring perf,$(KERNEL_DEFCONFIG)),)
-    ifeq ($(TARGET_BUILD_VARIANT),user)
-        PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-            ro.logdumpd.enabled=0
-    else
-        PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-            ro.logdumpd.enabled=1
-    endif
-else
-    PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-        ro.logdumpd.enabled=0
-endif
 
 #Healthd packages
 PRODUCT_PACKAGES += \
@@ -329,17 +247,6 @@ TARGET_MOUNT_POINTS_SYMLINKS := false
 # Disable skip validate
 PRODUCT_PROPERTY_OVERRIDES += \
   vendor.display.disable_skip_validate=1
-
-#-------------------------------------------------------------------------------
-# wlan specific
-#-------------------------------------------------------------------------------
-include device/qcom/wlan/sdm660_64/wlan.mk
-
-# For bringup
-WLAN_BRINGUP_NEW_SP := true
-DISP_BRINGUP_NEW_SP := true
-CAM_BRINGUP_NEW_SP := true
-SEC_USERSPACE_BRINGUP_NEW_SP := true
 
 #vendor prop to disable advanced network scanning
 PRODUCT_PROPERTY_OVERRIDES += \
