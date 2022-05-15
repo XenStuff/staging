4bc1ca53f2b07d6f26daf86bdb1f39b099d263e8
AmulyaX <amulya.b520@gmail.com>
2022-05-15 05:20:29 +0000

    lavender: Add treble configs
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- device.mk
+++ device.mk
@@ -207,10 +207,6 @@ PRODUCT_PACKAGES += \
 # MIDI feature
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
 
-PRODUCT_FULL_TREBLE_OVERRIDE := true
-
-PRODUCT_VENDOR_MOVE_ENABLED := true
-
 #for android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
